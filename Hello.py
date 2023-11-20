0d122888f47cdf07913e3e939f72a10c1f1ba51a
Ayomitunde Fafore <ayomitunde.fafore@snowflake.com>
2023-11-20 14:05:14 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -19,32 +19,21 @@ LOGGER = get_logger(__name__)
 
 
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    with open("./dataset-credentials.p8", "rb") as key:
+      p_key= serialization.load_pem_private_key(
+          key.read(),
+          password=None,
+          backend=default_backend()
+      )
 
-    st.write("# Welcome to Streamlit! 👋")
+    pkb = p_key.private_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption())
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    conn = st.experimental_connection("snowpark", private_key=pkb, role="readonly_role")
+    query = conn.query('select * from free_dataset_GZ1M6Z2R41Y.public.t_rbaseit limit 10;');
+    st.dataframe(query)
 
 
 if __name__ == "__main__":
